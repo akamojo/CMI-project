3bd73f68e03579767813eea5b9369df46f20f947
jells123 <julka.bedziechowska@gmail.com>
2019-05-04 18:27:16 +0100

    Add LuminanceExtractor class

---
+++ src/luminanceextractor.cpp
@@ -0,0 +1,41 @@
+#include "luminanceextractor.h"
+
+LuminanceExtractor::LuminanceExtractor() {
+}
+
+double LuminanceExtractor::calculatePixel(ofPixels pixels, int i, int j, int vidWidth, int nChannels) {
+    double red     = (double) pixels[(j * vidWidth + i) * nChannels    ];
+    double green   = (double) pixels[(j * vidWidth + i) * nChannels + 1];
+    double blue    = (double) pixels[(j * vidWidth + i) * nChannels + 2];
+
+    return rc * red + gc * green + bc * blue;
+}
+
+double LuminanceExtractor::calculate(ofVideoPlayer vidPlayer) {
+    double luminanceSum = 0.0;
+
+    ofPixels & pixels = vidPlayer.getPixels();
+    int vidWidth = pixels.getWidth();
+    int vidHeight = pixels.getHeight();
+    int nChannels = pixels.getNumChannels();
+
+    double currentLumi;
+    for (int i = 0; i < vidWidth; ++i) {
+        for (int j = 0; j < vidHeight; ++j) {
+            currentLumi = calculatePixel(pixels, i, j, vidWidth, nChannels);
+            luminanceSum += currentLumi;
+        }
+    }
+    return luminanceSum / (double)(vidWidth * vidHeight);
+}
+
+void LuminanceExtractor::convertPixels(ofPixels &inPixels, ofPixels &newPixels, int vidWidth, int vidHeight) {
+
+    double currentLumi;
+    for (int i = 0; i < vidWidth; ++i) {
+        for (int j = 0; j < vidHeight; ++j) {
+            currentLumi = calculatePixel(inPixels, i, j, vidWidth, 3);
+            newPixels[j * vidWidth + i] = (int) currentLumi;
+        }
+    }
+}
---
+++ src/luminanceextractor.h
@@ -0,0 +1,22 @@
+#ifndef LUMINANCEEXTRACTOR_H
+#define LUMINANCEEXTRACTOR_H
+
+#include <ofVideoPlayer.h>
+
+class LuminanceExtractor
+{
+public:
+    double calculate(ofVideoPlayer vidPlayer);
+
+    LuminanceExtractor();
+    void convertPixels(ofPixels &inPixels, ofPixels &newPixels, int vidWidth, int vidHeight);
+
+private:
+    const double rc = 0.2126;
+    const double gc = 0.7152;
+    const double bc = 0.0722;
+
+    double calculatePixel(ofPixels, int i, int j, int vidWidth, int nChannels);
+};
+
+#endif // LUMINANCEEXTRACTOR_H
