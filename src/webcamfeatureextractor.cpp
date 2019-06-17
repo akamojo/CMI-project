422238790de38229e148bac8ed499e53497dd857
Urszula_Barbara Walinska <urszula.walinska@student.put.poznan.pl>
2019-06-17 16:43:04 +0100

    cleaned version

--- src/webcamfeatureextractor.cpp
+++ src/webcamfeatureextractor.cpp
@@ -3,38 +3,18 @@
 WebcamFeatureExtractor::WebcamFeatureExtractor() {
 }
 
-void WebcamFeatureExtractor::setup()
+void WebcamFeatureExtractor::setup(std::string fileName)
 {
 	this->frameCounter = 0;
-	//this->luminance = -1.0;
 	this->prevHist = NULL;
 	this->rythm = 0.0;
 
-	//this->skipStep = -1;
-	//this->frameStep = -1;
-
-	/*this->avgColors.clear();
-	for (int i = 0; i < 3; i++) {
-		this->avgColors.push_back(-1.0);
-	}*/
-
-	webCamPreviewFaceFinder.setup("haarcascade_frontalface_default.xml");
+	webCamPreviewFaceFinder.setup(fileName);
 }
-
-
-/*double WebcamFeatureExtractor::getLuminance()
-{
-	return this->luminance;
-}
-
-vector<double> WebcamFeatureExtractor::getAvgColors()
-{
-	return avgColors;
-}*/
 
 double WebcamFeatureExtractor::getRythm()
 {
-	return rythm;	// / (double)(frameCounter / frameStep);
+	return rythm;
 }
 
 void WebcamFeatureExtractor::calculate(ofVideoGrabber vidGrabber) {
@@ -50,50 +30,11 @@ void WebcamFeatureExtractor::calculate(ofVideoGrabber vidGrabber) {
 }
 
 vector<ofxCvBlob> WebcamFeatureExtractor::detectFaces(ofVideoGrabber vidGrabber) {
+	ofImage colorImg; 
 	colorImg.setFromPixels(vidGrabber.getPixels());
 	webCamPreviewFaceFinder.findHaarObjects(colorImg);
 	return webCamPreviewFaceFinder.blobs;
 }
-
-/*vector<double> WebcamFeatureExtractor::calculatePixel(ofPixels pixels, int i, int j, int vidWidth, int nChannels) {
-	vector<double> colors;
-
-	for (int k = 0; k < 3; k++) {
-		colors.push_back((double)pixels[(j * vidWidth + i) * nChannels + k]);
-	}
-
-	return colors;
-}
-
-vector<double> WebcamFeatureExtractor::calculateFrame() {
-	double result[4] = { 0.0 };
-
-	ofPixels & pixels = videoPlayer.getPixels();
-	int vidWidth = pixels.getWidth();
-	int vidHeight = pixels.getHeight();
-	int nChannels = pixels.getNumChannels();
-
-	double currentLumi;
-	for (int i = 0; i < vidWidth; i += skipStep) {
-		for (int j = 0; j < vidHeight; j += skipStep) {
-			vector<double> currentColors = calculatePixel(pixels, i, j, vidWidth, nChannels);
-			currentLumi = rc * currentColors[0] + gc * currentColors[1] + bc * currentColors[2];
-
-			for (int k = 0; k < 3; k++) {
-				result[k] += currentColors[k];
-			}
-			result[3] += currentLumi;
-		}
-	}
-
-	for (int k = 0; k < 4; k++) {
-		result[k] /= (double)(vidWidth / skipStep * vidHeight / skipStep);
-	}
-
-	int n = sizeof(result) / sizeof(result[0]);
-	vector<double> vresult(result, result + n);
-	return vresult;
-}*/
 
 double WebcamFeatureExtractor::calculateDiffBetweenFrames(ofVideoGrabber vidGrabber) {
 
@@ -130,15 +71,3 @@ double WebcamFeatureExtractor::calculateDiffBetweenFrames(ofVideoGrabber vidGrab
 
 	return diff;
 }
-
-/*void WebcamFeatureExtractor::convertPixels(ofPixels &inPixels, ofPixels &newPixels, int vidWidth, int vidHeight) {
-
-	double currentLumi;
-	for (int i = 0; i < vidWidth; ++i) {
-		for (int j = 0; j < vidHeight; ++j) {
-			vector<double> colors = calculatePixel(inPixels, i, j, vidWidth, 3);
-			currentLumi = rc * colors[0] + gc * colors[1] + bc * colors[2];
-			newPixels[j * vidWidth + i] = (int)currentLumi;
-		}
-	}
-}*/
--- src/webcamfeatureextractor.h
+++ src/webcamfeatureextractor.h
@@ -1,56 +1,33 @@
 #ifndef WEBCAMFEATUREEXTRACTOR_H
 #define WEBCAMFEATUREEXTRACTOR_H
 
-#include <ofVideoPlayer.h>
 #include "ofMain.h"
 #include "ofxCvColorImage.h"
 #include "ofxCvGrayscaleImage.h"
 #include <ofxCvHaarFinder.h>
 
-#include <iostream>
-#include <vector>
-#include <math.h>
-
 class WebcamFeatureExtractor
 {
 public:
 	WebcamFeatureExtractor();
-	void setup();
-	//double getLuminance();
-	//vector<double> getAvgColors();
+	void setup(std::string);
 	double getRythm();
 
 	void calculate(ofVideoGrabber);
 	vector<ofxCvBlob> detectFaces(ofVideoGrabber);
 
 private:
-	//vector<double> calculateFrame();
 	double calculateDiffBetweenFrames(ofVideoGrabber);
 
-	/*void convertPixels(ofPixels &inPixels, ofPixels &newPixels, int vidWidth, int vidHeight);
-	vector <double> calculatePixel(ofPixels, int i, int j, int vidWidth, int nChannels);
-
-	const double rc = 0.2126;
-	const double gc = 0.7152;
-	const double bc = 0.0722;*/
-
-	//int skipStep; // 250 sampli per frame
-	//const int samplesPerFrame = 250;
-
 	int frameCounter;
-	int frameStep = 7; // 25 frame'ów
-	//const int framesPerVideo = 75;
+	int frameStep = 7;		// co 7 frame'ów
 
 	CvHistogram* prevHist = NULL;
 
 	double rythm = 0.0;
 	double rythmThreshold = 200;
-	//double luminance;
-	//vector<double> avgColors;
 
 	ofxCvHaarFinder webCamPreviewFaceFinder;
-	ofImage colorImg;
-
 };
 
 #endif // WEBCAMFEATUREEXTRACTOR_H
